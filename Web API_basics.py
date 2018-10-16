cf4ab2f4cef0b5a2d7cccf6c50d79f9d8f18fa05
raghurs49 <43571576+raghurs49@users.noreply.github.com>
2018-10-16 11:26:29 +0530

    Update Web API_basics.py

--- Web API_basics.py
+++ Web API_basics.py
@@ -1,1 +1,4 @@
-
+>>> import requests
+>>> response=requests.get(" https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+>>> print(response.content)
+b'?({"quoteText":"Fears are nothing more than a state of mind.  ", "quoteAuthor":"Napoleon Hill ", "senderName":"", "senderLink":"", "quoteLink":"http://forismatic.com/en/aaa95522fa/"})'
